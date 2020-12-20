3ed9d0fc515f5a78ae5d12dee7a6b2ff0cc6376b
souviknandi24 <souviknandi24@gmail.com>
2020-12-21 02:48:38 +0530

    added responsive menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BasicComponent } from './components/basic/basic.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonComponent } from './components/button/button.component';
+import { ButtonModule } from 'primeng/button';
 import { CalculusComponent } from './components/calculus/calculus.component';
 import { ConverterComponent } from './components/converter/converter.component';
 import { CurrencyComponent } from './components/currency/currency.component';
 import { FinanceComponent } from './components/finance/finance.component';
 import { GlassButtonComponent } from './components/glass-button/glass-button.component';
 import { GraphComponent } from './components/graph/graph.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
 import { MessageService } from 'primeng/api';
 import { NgModule } from '@angular/core';
 import { ScientificComponent } from './components/scientific/scientific.component';
@@ -31,7 +34,16 @@ import { ToastModule } from 'primeng/toast';
     FinanceComponent,
     GlassButtonComponent,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, TabMenuModule, ToastModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TabMenuModule,
+    ToastModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   providers: [MessageService],
   bootstrap: [AppComponent],
 })
